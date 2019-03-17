baf0b7a5ba97e7627ed83ebd4c3c60747ca967ad
Zsolt Gyorffy <zsoltgyorffy@gmail.com>
2019-03-17 18:49:34 +0200

    refined alghorithm

--- test_arrays/test_arrays.ino
+++ test_arrays/test_arrays.ino
@@ -18,23 +18,25 @@ long dC;
 //speed
 int velocity = 40; //start speed
 float vMin = 3000;
-float vMax;        
+float vMax;
 float vAvg;
 float spOld = 0;
 float spMax = 100;
-int spListRight[36] = {42,44,48,51,54,58,62,65,70,73,78,
-                  82,85,88,92,96,100,104,108,111,115,
-                  119,123,127,131,134,139,142,147,151,
-                  154,160,164,167,171,174};
+int spListRight[36] = {42, 44, 48, 51, 54, 58, 62, 65, 70, 73, 78,
+                       82, 85, 88, 92, 96, 100, 104, 108, 111, 115,
+                       119, 123, 127, 131, 134, 139, 142, 147, 151,
+                       154, 160, 164, 167, 171, 174
+                      };
 
-int spListLeft[36] = {46,49,52,55,58,61,65,68,72,75,79,
-                      83,87,91,95,99,103,107,110,114,118,
-                      122,126,130,134,137,141,145,149,153,
-                      157,161,165,169,173,176};
+int spListLeft[36] = {46, 49, 52, 55, 58, 61, 65, 68, 72, 75, 79,
+                      83, 87, 91, 95, 99, 103, 107, 110, 114, 118,
+                      122, 126, 130, 134, 137, 141, 145, 149, 153,
+                      157, 161, 165, 169, 173, 176
+                     };
 
-struct speedData{
+struct speedData {
   int sp;
-  int spPos;   
+  int spPos;
 };
 
 struct speedData spd;
@@ -77,81 +79,81 @@ void setup() {
   analogWrite(LPWM, 0);
 }
 
-struct speedData speedSearch(int speedList[], int speedMax){
-  
+struct speedData speedSearch(int speedList[], int speedMax) {
+
   struct speedData sd;
   int lo = 0;
   int hi = 35;
   int mid;
 
-  if (speedMax < speedList[lo]){
+  if (speedMax < speedList[lo]) {
     sd.sp = speedList[lo];
     sd.spPos = lo;
     return (sd);
   }
 
-  if (speedMax > speedList[hi]){
+  if (speedMax > speedList[hi]) {
     sd.sp = speedList[hi];
     sd.spPos = hi;
     return (sd);
   }
 
-  while (lo <= hi){
+  while (lo <= hi) {
     mid = (int)((lo + hi) / 2);
 
-    if (speedMax < speedList[mid]){
+    if (speedMax < speedList[mid]) {
       hi = mid - 1;
-    } else if (speedMax > speedList[mid]){
+    } else if (speedMax > speedList[mid]) {
       lo = mid + 1;
     } else {
       sd.sp = speedList[mid];
       sd.spPos = mid;
-      return(sd);
+      return (sd);
     }
   }
 
-  if (speedList[lo] - speedMax < speedMax - speedList[hi]){
+  if (speedList[lo] - speedMax < speedMax - speedList[hi]) {
     sd.sp = speedList[lo];
     sd.spPos = lo;
-    return(sd);
+    return (sd);
   } else {
     sd.sp = speedList[hi];
     sd.spPos = hi;
-    return(sd);
+    return (sd);
   }
 }
 
-void goRight(){
+void goRight() {
 
   Serial.println("here");
-  
+
   tStart = millis();
   tn = millis();
   i = 0;
   analogWrite(RPWM, spListRight[i]);
   newPosition = myEnc.read();
-  
+
   while ((tn <= (tStart + tT) && newPosition <= dMax)
-         || newPosition <= dMax){
+         || newPosition <= dMax) {
 
     newPosition = myEnc.read();
-    
-    if (tn <= (tStart + tA)){
-      if (millis() > tn + a){
+
+    if (tn <= (tStart + tA)) {
+      if (millis() > tn + a) {
         analogWrite(RPWM, spListRight[++i]);
         tn = millis();
         //Serial.print("Pos: ");
         //Serial.println(newPosition);
       }
     }
-    
+
     if ((tn > (tStart + tA))
-        && (tn < (tStart + tA + tC))){
-        tn = millis();
+        && (tn < (tStart + tA + tC))) {
+      tn = millis();
     }
-    
-    if ((tn >= (tStart + tA + tC)) && (newPosition >= dMax - (dC + dA)) && i > 0){
-      if (millis() > tn + a){
+
+    if ((tn >= (tStart + tA + tC)) && (newPosition >= dMax - (dC + dA)) && i > 0) {
+      if (millis() > tn + a) {
         analogWrite(RPWM, spListRight[--i]);
         tn = millis();
         //Serial.print("Pos: ");
@@ -164,35 +166,35 @@ void goRight(){
   //Serial.println(tn - tStart);
 }
 
-void goLeft(){
-  
+void goLeft() {
+
   tStart = millis();
   tn = millis();
   i = 0;
   analogWrite(LPWM, spListLeft[i]);
   newPosition = myEnc.read();
-  
+
   while ((tn <= (tStart + tT) && newPosition >= 0)
-          || newPosition >= 0){
+         || newPosition >= 0) {
 
     newPosition = myEnc.read();
-    
-    if (tn <= (tStart + tA)){
-      if (millis() > tn + a){
+
+    if (tn <= (tStart + tA)) {
+      if (millis() > tn + a) {
         analogWrite(LPWM, spListLeft[++i]);
         tn = millis();
         //Serial.print("Pos: ");
         //Serial.println(newPosition);
       }
     }
-    
+
     if ((tn > (tStart + tA))
-        && (tn < (tStart + tA + tC))){
-        tn = millis();
+        && (tn < (tStart + tA + tC))) {
+      tn = millis();
     }
-    
-    if ((tn >= (tStart + tA + tC)) && (newPosition <= dA) && i > 0){
-      if (millis() > tn + a){
+
+    if ((tn >= (tStart + tA + tC)) && (newPosition <= dA) && i > 0) {
+      if (millis() > tn + a) {
         analogWrite(LPWM, spListLeft[--i]);
         tn = millis();
         //Serial.print("Pos: ");
@@ -201,47 +203,51 @@ void goLeft(){
     }
   }
   analogWrite(LPWM, 0);
-  Serial.print("time: ");
-  Serial.println(tn - tStart);
+  //Serial.print("time: ");
+  //Serial.println(tn - tStart);
+}
+
+void setVariables() {
+  spOld = spMax;
+  spd = speedSearch(spListRight, spMax);
+  steps = spd.spPos;
+
+  vMax = steps * 500 + vMin;
+  vAvg = (vMin + vMax) / 2;
+  tA = steps * a;       // time to maximum speed
+  dA = vAvg * tA / 1000;
+  dC = (int)(dMax - (2 * dA)); //dB = dA
+  tC = (int)(dC / vMax * 1000); // time in C distance
+  tT = (int)(2 * tA + tC);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  if (spOld != spMax){
-    spOld = spMax;
-    spd = speedSearch(spListRight, spMax);
-    steps = spd.spPos;
-
-    vMax = steps * 500 + vMin;
-    vAvg = (vMin + vMax) / 2;
-    tA = steps * a;       // time to maximum speed
-    dA = vAvg * tA / 1000;
-    dC = (int)(dMax - (2 * dA)); //dB = dA
-    tC = (int)(dC / vMax * 1000); // time in C distance
-    tT = (int)(2 * tA + tC);
+  if (spOld != spMax) {
+    setVariables();
   }
- 
-   if (tStart == 0){
+
+  if (tStart == 0) {
     goRight();
     newPosition = myEnc.read();
     Serial.print("Pos: ");
     Serial.println(newPosition);
-    
+
     goLeft();
     newPosition = myEnc.read();
     Serial.print("Pos: ");
     Serial.println(newPosition);
-   
-   }
+
+  }
 
   /*
-  analogWrite(RPWM, 0);
-  analogWrite(LPWM, 0);
+    analogWrite(RPWM, 0);
+    analogWrite(LPWM, 0);
 
-  newPosition = myEnc.read();
-  
-  if (newPosition / 100 != oldPosition / 100) {
+    newPosition = myEnc.read();
+
+    if (newPosition / 100 != oldPosition / 100) {
     oldPosition = newPosition;
     Serial.println("*************************");
     Serial.print("Pos: ");
@@ -252,7 +258,7 @@ void loop() {
     Serial.println(millis());
     Serial.print("Duration: ");
     Serial.println(millis() - tStart);
-    Serial.println("*************************"); 
+    Serial.println("*************************");
     Serial.print("Speed: ");
     Serial.println(spd.sp);
     Serial.print("steps: ");
@@ -273,6 +279,6 @@ void loop() {
     Serial.println(tT);
     Serial.print("i: ");
     Serial.println(i);
-  }
+    }
   */
 }
